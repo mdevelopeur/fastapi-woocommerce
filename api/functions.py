0a86776159430300199719e856989ea5ed4fc57f
mdevelopeur <mdevelopeur@gmail.com>
2025-12-10 18:26:46 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -11,8 +11,8 @@ import random
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=".env.local")
-redis_url = os.getenv("REDIS_URL")
-api = os.getenv("api")
+cdek_account = os.getenv("cdek_account")
+cdek_secure_password = os.getenv("cdek_secure_password")
 target_store = 59
 eapi = "https://eapi.pcloud.com/"
 token = "AT2fZ89VHkDT7OaQZMlMlVkZdslpGwQPJNbTKpnbvQtbO8yBYcny"
@@ -41,146 +41,13 @@ async def main(deal):
       await confirm_documents(client, documents)
     client.close()
     
-async def get_products(client, deal):
-  url = api + "crm.deal.productrows.get"
-  body = {"id": deal}
-  response = await client.post(url, json=body)
-  response = response.json()
-  return response["result"]
-
-async def get_remaining_amounts(client, products):
-  url = api + "batch"
-  cmd = {}
-  for product in products:
-    cmd[product["PRODUCT_ID"]] = f"catalog.storeproduct.list?filter[product_id]={product["PRODUCT_ID"]}&order[]=amount"
-  body = {"cmd": cmd}
-  response = await client.post(url, json=body)
-  response = response.json()
-  responses = response["result"]["result"]
-  print(type(responses))
-  print(responses)
-  return responses
-
-async def create_documents(client, products):
-  for product in products:
-    ...
-async def create_document(client, type):
-  print("Creating document...")
-  url = api + "catalog.document.add"
-  fields = {"docType": type, "responsibleId": 1, "currency": "RUB", "COMMENTARY":""}
-  body = {"fields": fields}
-  response = await client.post(url, json=body)
+async def get_cdek_token(client):
+  url = "https://api.cdek.ru/v2/oauth/token"
+  data = {"client_id": cdek_account, "client_secret": cdek_secure_password, "grant_type": "client_credentials"}
+  response = client.post(url, data=data)
   response = response.json()
   print(response)
-  return response["result"]["document"]["id"]
+  return response["access_token"]
 
-async def add_products(client, products, documents):
-  print("Adding products...")
-  url = api + "batch" 
-  cmd = {}
-  for product in products:
-    for store in product["storeAmounts"]:
-      fields = {"storeTo": product["STORE_ID"], "elementId": product["PRODUCT_ID"], "amount": store["amount"], "purchasingPrice":1}
-      if store["store"] != -1:
-        print("-1: ", store)
-        fields["storeFrom"] = store["store"]
-        fields["docId"] = documents["M"]
-      else:
-        #print
-        fields["docId"] = documents["S"]
-      fields = get_fields_string(fields)
-      cmd[f"{product["ID"]}:{store["store"]}"] = f"catalog.document.element.add?{fields}"
-  body = {"cmd": cmd}
-  response = await client.post(url, json=body)
-  response = response.json()
-  responses = response["result"]["result"]
-  print(response)
-  return responses
+async def get_cdek_order_number(client, token):
   
-def get_fields_string(fields):
-  strings = []
-  for field in fields.keys():
-    strings.append(f"fields[{field}]={fields[field]}")
-    
-  return "&".join(strings)
-    
-def filter_remainings(data):
-  for key in data.keys():
-    data[key] = list(filter(lambda item: item["amount"] is not None, data[key]["storeProducts"]))
-  return data
-
-def process_product(product, remainings):
-  print("Product: ", product["PRODUCT_ID"])
-  product["storeAmounts"] = []
-  for store in remainings:
-    reserved = store["quantityReserved"]
-    
-    available = store["amount"] - reserved if reserved is not None else store["amount"]
-    if available is not None and available:
-      amount = product["QUANTITY"] if product["QUANTITY"] <= available else available
-      product["storeAmounts"].append({"product": product["PRODUCT_ID"], "store": store["storeId"], "amount": amount, "document": "M"})
-      product["QUANTITY"] -= amount 
-      #incomeDocRequired = True
-  if product["QUANTITY"] > 0:
-    product["storeAmounts"].append({"store": -1, "amount": product["QUANTITY"],  "document": "S"})
-  product["total"] = int(product["QUANTITY"])
-    #transferDocRequired = True 
-  return product 
-
-async def get_documents(client, products):
-  documents = {}
-  for product in products:
-    if "S" not in documents:
-      if list(filter(lambda store: store["store"] == -1, product["storeAmounts"])):     
-        document = await create_document(client, "S")
-        documents["S"] = document
-        print(documents)
-    if "M" not in documents:
-      if list(filter(lambda store: store["store"] != -1, product["storeAmounts"])):
-        document = await create_document(client, "M")
-        documents["M"] = document
-        print(documents)
-    if len(documents.keys()) > 1:
-      break
-
-  return documents 
-
-async def update_document(client, document, total):
-  print("Updating document...")
-  url = api + "catalog.document.update"
-  body = {"id": document, "fields": {"total": total}}
-  response = await client.post(url, json=body)
-  response = response.json()
-  responses = response["result"]
-  return responses
-
-async def confirm_documents(client, documents):
-  print("Confirming...")
-  url = api + "batch" 
-  cmd = {}
-  for document in documents.values():
-    cmd[document] = f"catalog.document.confirm?id={document}"
-  body = {"cmd": cmd}
-  response = await client.post(url, json=body)
-  response = response.json()
-  responses = response["result"]["result"]
-  print(responses)
-  return responses
-
-async def check_status(client, id):
-  statuses = {"0":"7", "4":"C4:UC_BZT361", "6":"C6:EXECUTING"}
-  url = api + f"crm.deal.get?id={id}"
-  body = {"id": int(id)}
-  print(body)
-  response = await client.get(url)
-  response = response.json()
-  print(response)
-  category = response["result"]["CATEGORY_ID"]
-  stage = response["result"]["STAGE_ID"]
-  if statuses[category] == stage:
-    return True
-  else: 
-    return False
-  #return response["result"]
-
-#async def get_status(client, deal)
