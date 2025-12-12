924b2cd6fb53617bc5fcfd3827c731a8e83be38c
mdevelopeur <mdevelopeur@gmail.com>
2025-12-12 15:52:58 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 import multipart
 import re
-from api.functions import create, update
+from api.functions import create, update, update_encoding
 from urllib.parse import unquote, urlparse
 
 app = FastAPI()
@@ -47,4 +47,14 @@ async def create_deal(request: Request):
     except Exception as e:
         print(e)
         return e
-    
+
+@app.post('/api/update_encoding')
+async def create_deal(request: Request):
+    try: 
+        data = await request.json()
+        print(data)
+        await update_encoding(data)
+        return 
+    except Exception as e:
+        print(e)
+        return e
